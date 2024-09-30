4c97f3f1cc5c02270836c9bf208b23a2ef7f7c98
KanishkaSharma18 <kanishkasharma.cs23@bmsce.ac.in>
2024-09-30 21:46:46 +0530

    Delete BoxDemo1.java

--- BoxDemo1.java
+++ BoxDemo1.java
@@ -1,18 +1,1 @@
-class Box
-{
-double width,height,depth;
-}
-class BoxDemo1
-{
-public static void main(String args[])
-{
-Box mybox=new Box();
-mybox.width=10;
-mybox.height=20;
-mybox.depth=30;
 
-double vol;
-vol=mybox.width*mybox.height*mybox.depth;
-System.out.println("Volume is "+vol);
-}
-}
